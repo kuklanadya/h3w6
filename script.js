0a21f759045de91ff145a6cb85f5e659f755f9b4
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-06 15:43:11 +0200

    final version

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 //Task 1
 function sumOfMin(array) {
-   let result = 0;
-   for (let i = 0; i < array.length; i++) {
-      result += array[i].sort((a, b) => a - b)[0];
-   }
-   return result;
+   return array.reduce((result, element) => result += element.sort((a, b) => a - b)[0], 0);
 }
 
 //Task 2
 function twoOldestAges(array) {
-   return array.sort((a, b) => a - b).slice(-2);
+   return [...new Set(array)].sort((a, b) => a - b).slice(-2);
 }
 
 //Task 3
 function shortestWord(string) {
-   return string.split(' ').sort((a, b) => a.length - b.length)[0].length;
+   return string.trim().split(' ').sort((a, b) => a.length - b.length)[0].length;
 }
 
 //Task 4
 function yourOrder(string) {
-   return string.split(' ').sort((a, b) => a.match(/\d/) - b.match(/\d/));
+   return string.split(' ').sort((a, b) => a.match(/\d/) - b.match(/\d/)).join(' ');
 }
